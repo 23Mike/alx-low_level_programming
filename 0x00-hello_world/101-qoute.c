c6473ea486ea65062dd0431314d94807d67075e2
23mike <mikeydave001@gmail.com>
2022-06-05 09:29:00 -0700

    C programming

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - print string in the function
- * Dscription: print "and that piece of art is useful"- Dora korpar, 2015-10-19"
+ * Description: "and that piece of art is useful"- Dora korpar, 2015-10-19"
  * Return: 0
  */
 int main(void)
 {
-printf("and that piece of art is useful"\" -Dora korpar, 2015-10-19\n");
 
-return (0)
+printf("and that piece of art is useful\n");
+
+printf("-Dora korpar, 2015-10-19\n");
+
+return (0);
 }
