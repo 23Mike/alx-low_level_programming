53dc5d87a905e375cd691033690aae38812e4470
23mike <mikeydave001@gmail.com>
2022-06-05 06:26:47 -0700

    C programming

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print string in the function
+ * Dscription: print "and that piece of art is useful"- Dora korpar, 2015-10-19"
+ * Return: 0
+ */
+int main(void)
+{
+printf("and that piece of art is useful"\" -Dora korpar, 2015-10-19\n");
+
+return (0)
+}
