7665f2f25d022e9b3464ed544ba438e3ea9933d3
23Mike <mikeydave001@gmail.com>
2022-09-20 19:37:36 -0700

    C program that reverses a string

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls other funcs
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	void _puts(char *str);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls other funcs
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	void print_rev(char *S);
+
+	return (0);
+}
