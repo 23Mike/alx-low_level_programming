58ed68486e6ebcf7b04180cb9c718c3d7912bfbb
23Mike <mikeydave001@gmail.com>
2022-10-26 14:02:49 -0700

    C - program that prints all the elements of list

---
+++ 0x13-more_singly_linked_lists/5-main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 0);
+	print_listint(head);
+	printf("p\n", (void *)head);
+	return (0);
+}
